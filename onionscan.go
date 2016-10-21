fd7c9160d4cb44212d8a9d9638e267932b9767de
Scott Ainslie <scott@scottainslie.me.uk>
2016-10-20 21:05:02 -0400

    Remote Desktop Protocol

--- onionscan.go
+++ onionscan.go
@@ -1,15 +1,83 @@
-package main
+package onionscan
 
 import (
+	"fmt"
 	"github.com/s-rah/onionscan/config"
 	"github.com/s-rah/onionscan/protocol"
 	"github.com/s-rah/onionscan/report"
 	"github.com/s-rah/onionscan/utils"
 	"strings"
+	"time"
 )
 
 type OnionScan struct {
-	Config *config.OnionscanConfig
+	Config *config.OnionScanConfig
+}
+
+func (os *OnionScan) GetAllActions() []string {
+	return []string{
+		"web",
+		"tls",
+		"ssh",
+		"irc",
+		"rdp",
+		"ricochet",
+		"ftp",
+		"smtp",
+		"mongodb",
+		"vnc",
+		"xmpp",
+		"bitcoin",
+		"bitcoin_test",
+		"litecoin",
+		"dogecoin",
+	}
+}
+
+func (os *OnionScan) PerformNextAction(report *report.OnionScanReport, nextAction string) error {
+	switch nextAction {
+	case "web":
+		wps := new(protocol.HTTPProtocolScanner)
+		wps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "tls":
+		tps := new(protocol.TLSProtocolScanner)
+		tps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "ssh":
+		sps := new(protocol.SSHProtocolScanner)
+		sps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "irc":
+		ips := new(protocol.IRCProtocolScanner)
+		ips.ScanProtocol(report.HiddenService, os.Config, report)
+	case "rdp":
+		ips := new(protocol.RDPProtocolScanner)
+		ips.ScanProtocol(report.HiddenService, os.Config, report)
+	case "ricochet":
+		rps := new(protocol.RicochetProtocolScanner)
+		rps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "ftp":
+		fps := new(protocol.FTPProtocolScanner)
+		fps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "smtp":
+		smps := new(protocol.SMTPProtocolScanner)
+		smps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "mongodb":
+		mdbps := new(protocol.MongoDBProtocolScanner)
+		mdbps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "vnc":
+		vncps := new(protocol.VNCProtocolScanner)
+		vncps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "xmpp":
+		xmppps := new(protocol.XMPPProtocolScanner)
+		xmppps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "bitcoin", "bitcoin_test", "litecoin", "litecoin_test", "dogecoin", "dogecoin_test":
+		bps := protocol.NewBitcoinProtocolScanner(nextAction)
+		bps.ScanProtocol(report.HiddenService, os.Config, report)
+	case "none":
+		return nil
+	default:
+		return fmt.Errorf("Unknown scanner %s", nextAction)
+	}
+	return nil
 }
 
 func (os *OnionScan) Scan(hiddenService string, out chan *report.OnionScanReport) {
@@ -23,45 +91,22 @@ func (os *OnionScan) Scan(hiddenService string, out chan *report.OnionScanReport
 
 	report := report.NewOnionScanReport(hiddenService)
 
-	// HTTP
-	hps := new(protocol.HTTPProtocolScanner)
-	hps.ScanProtocol(hiddenService, os.Config, report)
-
-	// SSH
-	sps := new(protocol.SSHProtocolScanner)
-	sps.ScanProtocol(hiddenService, os.Config, report)
-
-	// Ricochet
-	rps := new(protocol.RicochetProtocolScanner)
-	rps.ScanProtocol(hiddenService, os.Config, report)
-
-	// Bitcoin
-	bps := new(protocol.BitcoinProtocolScanner)
-	bps.ScanProtocol(hiddenService, os.Config, report)
-
-	//IRC
-	ips := new(protocol.IRCProtocolScanner)
-	ips.ScanProtocol(hiddenService, os.Config, report)
-
-	//FTP
-	fps := new(protocol.FTPProtocolScanner)
-	fps.ScanProtocol(hiddenService, os.Config, report)
-
-	//SMTP
-	smps := new(protocol.SMTPProtocolScanner)
-	smps.ScanProtocol(hiddenService, os.Config, report)
-
-	//MongoDb
-	mdbps := new(protocol.MongoDBProtocolScanner)
-	mdbps.ScanProtocol(hiddenService, os.Config, report)
-
-	//VNC
-	vncps := new(protocol.VNCProtocolScanner)
-	vncps.ScanProtocol(hiddenService, os.Config, report)
-
-	//XMPP
-	xmppps := new(protocol.XMPPProtocolScanner)
-	xmpps.ScanProtocol(hiddenService, os.Config, report)
+	for _, nextAction := range os.Config.Scans {
+		err := os.PerformNextAction(report, nextAction)
+		if err != nil {
+			os.Config.LogInfo(fmt.Sprintf("Error: %s", err))
+		} else {
+			report.PerformedScans = append(report.PerformedScans, nextAction)
+		}
+		if time.Now().Sub(report.DateScanned).Seconds() > os.Config.Timeout.Seconds() {
+			report.TimedOut = true
+		}
+	}
+	if len(report.PerformedScans) != 0 {
+		report.NextAction = report.PerformedScans[len(report.PerformedScans)-1]
+	} else {
+		report.NextAction = "none"
+	}
 
 	out <- report
 }
